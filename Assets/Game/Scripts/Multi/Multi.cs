92521f97b74ab7c91dddec8d18c634c743601391
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-25 14:52:29 +0900

    サーバー側のレシーブ機能作った

--- Assets/Game/Scripts/Multi/Multi.cs
+++ Assets/Game/Scripts/Multi/Multi.cs
@@ -12,29 +12,64 @@ namespace KYapp.Builate
     {
         private string host;
         private int port;
-        private UdpClient client;
+        private UdpClient UdpClient;
 
         public MultiClient(string host,int port)
         {
             this.host = host;
             this.port = port;
 
-            client = new UdpClient();
-            client.Connect(host, port);
+            UdpClient = new UdpClient();
+            UdpClient.Connect(host, port);
         }
 
         public byte[] Send(byte[] ms)
         {
-            client.Send(ms, ms.Length);
+            UdpClient.Send(ms, ms.Length);
 
             IPEndPoint ServerEp = new IPEndPoint(IPAddress.Any, 0);
-            byte[] resbyte = client.Receive(ref ServerEp);
+            byte[] resbyte = UdpClient.Receive(ref ServerEp);
 
             return resbyte;
+        }
+
+        public void Close()
+        {
+            UdpClient.Close();
         }
     }
     public class MultiServer
     {
+        private int port;
+        private UdpClient UdpClient;
+        private Server_GetByte server_GetByte;
 
+        public MultiServer(int port, Server_GetByte server_GetByte)
+        {
+            this.port = port;
+            this.server_GetByte = server_GetByte;
+            UdpClient = new UdpClient(port);
+            UdpClient.BeginReceive(Receive, UdpClient);
+        }
+
+        public void Receive(IAsyncResult result)
+        {
+            UdpClient getUdp = (UdpClient)result.AsyncState;
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] getByte = getUdp.EndReceive(result, ref ipEnd);
+
+            byte[] msg = server_GetByte(getByte);
+
+            getUdp.Send(msg, msg.Length, ipEnd);
+            getUdp.BeginReceive(Receive, getUdp);
+        }
+
+        public void Close()
+        {
+            UdpClient.Close();
+        }
     }
+
+    public delegate byte[] Server_GetByte(byte[] ClientMsg);
 }
